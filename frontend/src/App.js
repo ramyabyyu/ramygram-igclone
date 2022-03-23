f0ac7fbc39874ed221e1f8b397fd6152c3072ff9
ramyabyyu <bintantramy@gmail.com>
2022-03-23 11:29:38 +0700

    Designing Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as Path from "./routeNames";
+import { Container } from "react-bootstrap";
 
-function App() {
+// components
+import AppNavbar from "./components/AppNavbar";
+
+// Pages
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <AppNavbar />
+        <Container>
+          <Routes>
+            <Route path={Path.HOME} element={<Home />} />
+            <Route path={Path.AUTH} element={<Auth />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/AppNavbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import * as Path from "../routeNames";
+
+const AppNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="mb-5">
+      <Container>
+        <Navbar.Brand href={Path.HOME} className="brand">
+          RamyGram
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href={Path.HOME}>Home</Nav.Link>
+            <Nav.Link href={Path.AUTH}>Sign In</Nav.Link>
+            <Nav.Link href={Path.PROFILE}>Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
